f4fa14b017634b9bba928d244c36c928e3b37587
Felipe Batista <felipe.c.b@hotmail.com>
2021-11-18 14:14:00 -0300

    solucao sem utilizacao de interface

---
+++ Exercicio interfaces/Entities/Invoice.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace Exercicio_interfaces.Entities
+{
+    class Invoice
+    {
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+
+        public Invoice(double basicPayment, double tax)
+        {
+            BasicPayment = basicPayment;
+            Tax = tax;
+        }
+
+        public double TotalPayment { get { return BasicPayment + Tax; } }
+
+        public override string ToString()
+        {
+            return "Basic Payment: "
+                + BasicPayment.ToString("f2", CultureInfo.InvariantCulture)
+                + "\nTax: "
+                + Tax.ToString("F2", CultureInfo.InvariantCulture)
+                + "\nTotal payment: "
+                + TotalPayment.ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+    }
+}
---
+++ Exercicio interfaces/Entities/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿namespace Exercicio_interfaces.Entities
+{
+    class Vehicle
+    {
+        public string Model { get; set; }
+
+        public Vehicle(string model)
+        {
+            Model = model;
+        }
+    }
+}
